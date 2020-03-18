831b3d1fa03a22b4163fe83b4d9c6f4e4797622c
reuelrds <reuelrds@gmail.com>
2020-03-19 01:48:25 +0530

    Added a range pipa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RangePipe } from './range.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RangePipe
   ],
   imports: [
     BrowserModule
