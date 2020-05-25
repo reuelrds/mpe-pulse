01a7650684cffae21c012575bd8cfcf275c01e37
reuelrds <reuelrds@gmail.com>
2020-05-25 10:21:54 +0530

    Added App routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { BaseComponent } from './base/base.component';
+import { HeroComponent } from './hero/hero.component';
 
 
 const routes: Routes = [
-  { path: '', component: BaseComponent },
+  { path: '', component: HeroComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HeroComponent } from './hero/hero.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeroComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
