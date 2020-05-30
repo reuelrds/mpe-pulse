260c00f88701af06e31693e5ade4fb68e877eed2
Reuel D'silva <reuelrds@gmail.com>
2020-05-30 10:28:52 +0530

    Init About Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from './about/about.component';
 
 
 const routes: Routes = [
   { path: '', component: HeroComponent },
+  { path: 'about', component: AboutComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeroComponent } from './hero/hero.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroComponent,
     NavbarComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
