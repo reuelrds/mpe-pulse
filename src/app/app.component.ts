5a4b0d46e904aa8985e7bdd7ccec669d1049e266
Reuel D'silva <reuelrds@gmail.com>
2020-05-30 12:01:05 +0530

    Fixed Navbar Box Shadow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'marsh-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'marsh';
+  shadow = 'dark';
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (this.router.url === '/') {
+        this.shadow = 'dark';
+      } else {
+        this.shadow = 'light';
+      }
+    });
+  }
 }
