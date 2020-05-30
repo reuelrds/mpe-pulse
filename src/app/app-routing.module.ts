8595243e354c9bcdb0b56d6cc55557db2e87a7dd
Reuel D'silva <reuelrds@gmail.com>
2020-05-30 11:51:15 +0530

    Added Capability Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HeroComponent } from './hero/hero.component';
 import { AboutComponent } from './about/about.component';
+import { CapabilityComponent } from './capability/capability.component';
 
 
 const routes: Routes = [
   { path: '', component: HeroComponent },
-  { path: 'about', component: AboutComponent }
+  { path: 'about', component: AboutComponent },
+  { path: 'capability', component: CapabilityComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { HeroComponent } from './hero/hero.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AboutComponent } from './about/about.component';
+import { CapabilityComponent } from './capability/capability.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { AboutComponent } from './about/about.component';
     HeroComponent,
     NavbarComponent,
     AboutComponent,
+    CapabilityComponent,
   ],
   imports: [
     BrowserModule,
