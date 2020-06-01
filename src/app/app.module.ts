9234017b63f8bbe6a701bdb0cc7e903ad9d52270
Reuel D'silva <reuelrds@gmail.com>
2020-06-01 12:23:44 +0530

    Added contact form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -17,16 +23,20 @@ import { SampleComponent } from './sample/sample.component';
   declarations: [
     AppComponent,
     HeroComponent,
-    NavbarComponent,
     AboutComponent,
-    CapabilityComponent,
     QuoteComponent,
-    EquipmentComponent,
+    NavbarComponent,
     SampleComponent,
+    EquipmentComponent,
+    CapabilityComponent,
   ],
   imports: [
     BrowserModule,
+    MatInputModule,
+    MatButtonModule,
     AppRoutingModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule
   ],
   providers: [],
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormBuilder, FormGroup } from '@angular/forms';
+
 @Component({
   selector: 'marsh-quote',
   templateUrl: './quote.component.html',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteComponent implements OnInit {
 
-  constructor() { }
+  contactForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.contactForm = this.formBuilder.group({
+      name: '',
+      company: '',
+      address: '',
+      phone: '',
+      email: '',
+      subject: '',
+      message: ''
+    });
+  }
+
+  submitForm() {
+    console.log(this.contactForm.value);
   }
 
 }
